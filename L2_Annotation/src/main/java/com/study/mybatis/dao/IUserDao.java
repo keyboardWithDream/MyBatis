c4737da49abff405277da4fe7689aead573f6bc1
keyboardWithDream <isharlan.hu@gmail.com>
2020-08-04 17:11:23 +0800

    Update

---
+++ L2_Annotation/src/main/java/com/study/mybatis/dao/IUserDao.java
@@ -0,0 +1,20 @@
+package com.study.mybatis.dao;
+
+import com.study.mybatis.domain.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * @author Harlan
+ * @date 2020/8/3 20:18
+ */
+public interface IUserDao {
+
+    /**
+     * 查询所有用户
+     * @return 用户list
+     */
+    @Select("SELECT * FROM user")
+    List<User> findAll();
+}
